ade2d351bd437d62285ff571d2f58686e95d9917
Pouriya Tajik <pouriya.un@gmail.com>
2020-08-09 20:12:15 +0430

    Updates

---
+++ precache-manifest.f3d6a668236724cbc8be5f12c314e018.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a12e0f5e5aa42d0e370eccd46d1a5ac",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4a07e67af9e5366684ac",
+    "url": "/monsters-rolodex/static/css/main.1a331ecf.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a07e67af9e5366684ac",
+    "url": "/monsters-rolodex/static/js/main.b60c626d.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
